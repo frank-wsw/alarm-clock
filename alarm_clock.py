53f822e8027b18bf4625fe823cebce0f7a2cd0e1
frank-wsw <suwenw2@outlook.com>
2023-06-03 17:29:10 +0800

    add alarm() function

--- alarm_clock.py
+++ alarm_clock.py
@@ -1,5 +1,19 @@
 # alarm source: https://www.fesliyanstudios.com/royalty-free-sound-effects-download/alarm-203
 
 from playsound import playsound
+import time
 
-playsound("alarm.mp3")
+def alarm(seconds):
+    time_elapsed = 0
+
+    while time_elapsed < seconds:
+        time.sleep(1)
+        time_elapsed += 1
+
+        time_left = seconds - time_elapsed
+        minutes_left = time_left // 60
+        seconds_left = time_left % 60
+
+        print(f"{minutes_left}:{seconds_left}")
+
+alarm(10)
